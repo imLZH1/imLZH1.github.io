d3c57cf3c187483a728bc3809e6ff60f6e94684b
imLZH1 <qingwachong@qq.com>
2025-02-26 14:40:58 +0800

    Firs Push

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1739192078';
+const cacheName = 'chirpy-1740551934';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
